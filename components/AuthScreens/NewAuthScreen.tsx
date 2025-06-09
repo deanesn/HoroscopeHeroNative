52d818a43bb6eda422aa68a1d194578634b2ec2d
deanesn <132375544+deanesn@users.noreply.github.com>
2025-06-09 16:10:11 -0500

    Fix syntax error in NewAuthScreen.tsx

--- components/AuthScreens/NewAuthScreen.tsx
+++ components/AuthScreens/NewAuthScreen.tsx
@@ -214,7 +214,7 @@ export const NewAuthScreen = () => {
                   style={styles.primaryButtonGradient}
                 >
                   {loading ? (
-                    <ActivityIndicator color="#FFFFFF\" size=\"small" />
+                    <ActivityIndicator color="#FFFFFF" size="small" />
                   ) : (
                     <Text style={styles.primaryButtonText}>
                       {isSignUp ? 'Create Account' : 'Sign In'}
